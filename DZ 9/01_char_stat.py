277db68f42cfb4eae97f1f03c0ac6c35383baeb6
Vakaram <Vakaramm@gmail.com>
2022-07-12 01:07:51 +0500

    начал переводить всё в функции и классы

--- DZ 9/01_char_stat.py
+++ DZ 9/01_char_stat.py
@@ -30,7 +30,8 @@
 #             if i in unicl_znch:
 #                 pass
 #             else:
-#                 unicl_znch.append(i)
+#                 unicl_znch.append
+#                 (i)
 # # print('уникальные значения = ' , unicl_znch)
 
 # # # После выполнения первого этапа нужно сделать упорядочивание статистики
@@ -38,77 +39,122 @@
 # #  - по алфавиту по возрастанию
 # #  - по алфавиту по убыванию
 # # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
-#
-# stat = {}
-# count_bukv = 0
-# bukvas = 0
+import re
+# voina_i_mir ='voina-i-mir.txt'
+# stat ={}
 # with open(voina_i_mir, 'r',encoding='utf-8') as file:
 #     for line in file:
-#         for i in unicl_znch:
-#             bukvas = i
-#             for i in line:
-#               if bukvas == i:
-#                     count_bukv += 1
-#         stat[bukvas] = count_bukv
+#         for i in line:
+#             if i.isalpha():
+#                 if not i in stat:
+#                    stat[i] = 0
+#                 else:
+#                     stat[i] += 1
+# print(stat)
+# #считали сколько всего повторяются буквы
+# print('+----------+----------+')
+# print('|  буква   |  частота |')
+# print('+----------+----------+')
+# stat_sortA = {}
+# #сортируем словарь по А-Я
+# for i in sorted(stat):
+#     if re.search(r'[А-Я]',i):
+#         stat_sortA[i]=stat[i]
+# #принтуем словарь в красивом виде А-Я
+# for bukva,count in stat_sortA.items():
+#     print(f'|{bukva:^10}|{count:^10}|'.format(bukva=bukva,count=count))
+# #сортируем словарь по а-я
+# print('+----------+----------+')
+# stat_sorta = {}
+# for i in sorted(stat):
+#     if re.search(r'[а-я]',i):
+#         stat_sorta[i]=stat[i]
 #
-# with open(voina_i_mir, 'r',encoding='utf-8') as file:
-#     text = file.read()
+# #принтуем словарь в красивом виде А-Я
+# for bukva,count in stat_sorta.items():
+#     print(f'|{bukva:^10}|{count:^10}|'.format(bukva=bukva,count=count))
 #
-#     array = {}
-#     for i in text:
-#         if not i in array:
-#             array[i] = 0
-#         else:
-#             array[i] += 1
+# #Считаем итого
+# itogo = 0
+# for i in stat_sortA:
+#     itogo = itogo + stat_sortA[i]
+# for i in stat_sorta:
+#     itogo = itogo + stat_sorta[i]
 #
-# if '\n' in array: del array['\n']
-# print(array)
-import re
-voina_i_mir ='voina-i-mir.txt'
-
-stat ={}
-with open(voina_i_mir, 'r',encoding='utf-8') as file:
-    for line in file:
-        for i in line:
-            if i.isalpha():
-                if not i in stat:
-                   stat[i] = 0
-                else:
-                    stat[i] += 1
-#считали сколько всего повторяются буквы
-print('+----------+----------+')
-print('|  буква   |  частота |')
-print('+----------+----------+')
-stat_sortA = {}
-#сортируем словарь по А-Я
-for i in sorted(stat):
-    if re.search(r'[А-Я]',i):
-        stat_sortA[i]=stat[i]
-#принтуем словарь в красивом виде А-Я
-for bukva,count in stat_sortA.items():
-    print(f'|{bukva:^10}|{count:^10}|'.format(bukva=bukva,count=count))
-#сортируем словарь по а-я
-print('+----------+----------+')
-stat_sorta = {}
-for i in sorted(stat):
-    if re.search(r'[а-я]',i):
-        stat_sorta[i]=stat[i]
-
-#принтуем словарь в красивом виде А-Я
-for bukva,count in stat_sorta.items():
-    print(f'|{bukva:^10}|{count:^10}|'.format(bukva=bukva,count=count))
-
-itogo = 0
-for i in stat_sortA:
-    itogo = itogo + stat_sortA[i]
-for i in stat_sorta:
-    itogo = itogo + stat_sorta[i]
-
-print('+----------+----------+')
-print('|   итого  |{itogo:^10}|'.format(itogo = itogo))
-print('+----------+----------+')
+# print('+----------+----------+')
+# print('|   итого  |{itogo:^10}|'.format(itogo = itogo))
+# print('+----------+----------+')
 
 
+#___________________________________________
+
+
+
+class ThisFile:
+    import re
+    def __init__(self,name_file,encoding = 'utf-8'): #файл по умолчанию
+        self.name_file = name_file
+        self.codirovka = encoding
+        self.stat = {}
+
+    def stat_count(self): #считает статистику
+        with open(self.name_file , 'r',encoding = self.codirovka) as file:
+            for line in file:
+                for i in line:
+                    if i.isalpha():
+                        if not i in self.stat:
+                            self.stat[i] = 0
+                        else:
+                            self.stat[i] += 1
+
+    def __str__(self):
+        return f'Вы проверяете документ {self.name_file}\n' \
+               f'в кодирове {self.codirovka} по умолчанию,если ошибка поменяйте кодировку\n' \
+               f'+----------+----------+\n'\
+               f'|  буква   |  частота |\n' \
+               f'+----------+----------+\n' \
+               f'{self.sortirovkAdef()}\n' \
+
+    def sortirovkAdef(self):
+        import re
+        self.stat_sortA = {}
+        for i in sorted(self.stat):
+            if re.search(r'[А-Я]',i):
+                self.stat_sortA[i]=self.stat[i]
+            for bukva, count in self.stat_sortA.items():
+                self.sortirovkA = f'|{bukva:^10}|{count:^10}|'.format(bukva=bukva,count=count)
+                print(self.sortirovkA)
+
+
+proverka_file = ThisFile(name_file='voina-i-mir.txt')
+proverka_file.stat_count()
+proverka_file.sortirovkAdef()
+print(proverka_file)
+
+# #принтуем словарь в красивом виде А-Я
+
+# #сортируем словарь по а-я
+# print('+----------+----------+')
+# stat_sorta = {}
+# for i in sorted(stat):
+#     if re.search(r'[а-я]',i):
+#         stat_sorta[i]=stat[i]
+#
+# #принтуем словарь в красивом виде А-Я
+# for bukva,count in stat_sorta.items():
+#     print(f'|{bukva:^10}|{count:^10}|'.format(bukva=bukva,count=count))
+#
+# #Считаем итого символов
+# itogo = 0
+# for i in stat_sortA:
+#     itogo = itogo + stat_sortA[i]
+# for i in stat_sorta:
+#     itogo = itogo + stat_sorta[i]
+#
+# print('+----------+----------+')
+# print('|   итого  |{itogo:^10}|'.format(itogo = itogo))
+# print('+----------+----------+')
+#
 
 
 
