8d910ba0d2f550ad8eef788f2eb73a998666f98f
Vakaram <Vakaramm@gmail.com>
2022-07-15 23:57:32 +0500

    в общем оставил 5 файлов надо научиться доставать именно дату съёмки

--- DZ 9/03_files_arrange.py
+++ DZ 9/03_files_arrange.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os, time, shutil
+
 
 # Нужно написать скрипт для упорядочивания фотографий (вообще любых файлов)
 # Скрипт должен разложить файлы из одной папки по годам и месяцам в другую.
@@ -43,9 +43,7 @@ import os, time, shutil
 
 #логика
 #сначало заходим в папку, и начинаем смотреть каждый файл.
-#У файла смотрим дату создания дата равна например 2021.10.17 значит создаём папку берём год ,в папке
-#создаём год , в ней уже создаём месяц , в ниё а копируем файл.
-#Далее смотрим другой файл и его дату , этот файл 2021.10.23 ищим папку года, если есть ,
+#У файла смотрим дату Далее смотрим другой файл и его дату , этот файл 2021.10.23 ищим папку года, если есть ,
 #заходим в ниё и смотрим месяц если опять совпадение просто копируем файл в папу, если нет совпадений создаём
 # папу месяца и туда копируем файл.
 #если её нет то создаём папку 2021 и в ниё добавляем месяц соответствующий и копируем туда наш файл.
@@ -62,23 +60,36 @@ class FasovkaFilov:
         self.folder = folder
 
     def look_data(self):#смотрит дату у каждого файла построчно и смотрит или создаёт новую папку на выход
+        from PIL import Image
         import os
         from datetime import datetime
-        from datetime import date
         import shutil
         self.papka_open = os.listdir(self.folder)
-        new_papka = 'C://sort_papka'
-        # os.mkdir(new_papka)  # создаёт папку
-        print('мы создали папку ', new_papka)
+        new_papka = 'C:\\sort_papka' # добавляем название папки
+        # os.mkdir(new_papka)  # создаёт папку #наверное не нужна уже так как есть метод os.makedirs(add_papka)
+        # print('мы создали папку ', new_papka)
         for file in self.papka_open:
             put_k_file = self.folder+'\\'+file #путь к файлу с файлом
             data_file = os.path.getctime(put_k_file)
-            data_g_m_d = datetime.fromtimestamp(data_file).strftime('%y.%m.%d')
-            print(data_g_m_d) #мы счиали дату каждого файла
+            # data_g_m_d = datetime.fromtimestamp(data_file).strftime('%y.%m.%d')
+            # data_g_m_d = datetime.fromtimestamp(data_file)
+            # print(data_g_m_d)
+            i = Image.getdata(data_file)
+            print(i)
+            # papka_year_proverka = new_papka+'\\'+datetime.fromtimestamp(data_file).strftime('%y')
+            # add_papka = papka_year_proverka #создаём путь по первому файлу (пока год,потом месяц)
+            # if os.path.exists(papka_year_proverka):
+            #     print('такая папкаесть тут ничё не делаем')
+            #     shutil.copy(put_k_file, new_papka)
+            # else:
+            #     os.makedirs(add_papka)
+            #     print('мы создали папку', papka_year_proverka)
+
+            # if os.path.exists('сюда путь ')
+            # print(os.path.exists(papka_year_proverka)) #мы счиали дату каждого файла
             # print(self.folder+'\\'+file)
-            shutil.copyfile(put_k_file, new_papka) #копирует файл в раннее созданую папку
+            # shutil.copy(put_k_file, new_papka) #копирует файл в раннее созданую папку
             #тут сейчас думаю как запускать код от имени администратора?
-            #читать вот тут https://stackoverflow.com/questions/7518067/python-ioerror-errno-13-permission-denied-when-im-copying-file
 
     def remainder(self):# остаток файлов ( 120 из 453) и принтовать как файл обработается
         pass
@@ -86,6 +97,7 @@ class FasovkaFilov:
     def __str__(self):
         return f'{self.papka_open}'
 
-papka = FasovkaFilov(folder='C:\\Users\\admin\\Downloads\\Wallpapers-Assorti17')
+# papka = FasovkaFilov(folder='C:\\Users\\admin\\Downloads\\Wallpapers-Assorti17') #для ноута
+papka = FasovkaFilov(folder='C:\\Users\Vakaram\\Downloads\\Wallpapers-Assorti17') #для компа
 papka.look_data()
 print(papka)
