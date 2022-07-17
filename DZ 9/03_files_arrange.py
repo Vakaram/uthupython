79ff7f86b2936f020eac13a7b29ca0977550f63e
Vakaram <Vakaramm@gmail.com>
2022-07-17 22:35:00 +0500

    Сделал 3 задачку

--- DZ 9/03_files_arrange.py
+++ DZ 9/03_files_arrange.py
@@ -61,38 +61,60 @@ class FasovkaFilov:
 
     def look_data(self):#смотрит дату у каждого файла построчно и смотрит или создаёт новую папку на выход
         from PIL import Image
+        import exif
+        from exif import Image
         import os
-        from datetime import datetime
         import shutil
+        import sys
+        from PIL import Image
+        from PIL.ExifTags import TAGS
+        from datetime import datetime
         self.papka_open = os.listdir(self.folder)
         new_papka = 'C:\\sort_papka' # добавляем название папки
         # os.mkdir(new_papka)  # создаёт папку #наверное не нужна уже так как есть метод os.makedirs(add_papka)
         # print('мы создали папку ', new_papka)
         for file in self.papka_open:
-            put_k_file = self.folder+'\\'+file #путь к файлу с файлом
-            data_file = os.path.getctime(put_k_file)
-            # data_g_m_d = datetime.fromtimestamp(data_file).strftime('%y.%m.%d')
-            # data_g_m_d = datetime.fromtimestamp(data_file)
-            # print(data_g_m_d)
-            i = Image.getdata(data_file)
-            print(i)
-            # papka_year_proverka = new_papka+'\\'+datetime.fromtimestamp(data_file).strftime('%y')
-            # add_papka = papka_year_proverka #создаём путь по первому файлу (пока год,потом месяц)
-            # if os.path.exists(papka_year_proverka):
-            #     print('такая папкаесть тут ничё не делаем')
-            #     shutil.copy(put_k_file, new_papka)
+            self.put_k_file = self.folder+'\\'+file #путь к файлу с файлом .jpeg
+            self.data_create_def()
+            papka_year_proverka = new_papka+'\\'+self.data_sozdaniya_year
+            papka_year_mounth_proverka = papka_year_proverka+'\\'+self.data_sozdaniya_mounth
+            if not os.path.exists(papka_year_mounth_proverka):
+                os.makedirs(papka_year_mounth_proverka) #создаём папку по го и месяц если такой нет
+            shutil.copy(self.put_k_file, papka_year_mounth_proverka) #копируем файл в новую директорию
+            # if os.path.exists(papka_year_mounth_proverka):
+            #         print('такой путь уже есть и я просто копирую в него файл')
+            #         shutil.copy(self.put_k_file, papka_year_mounth_proverka)#копируем файл в существующию директорию
             # else:
-            #     os.makedirs(add_papka)
-            #     print('мы создали папку', papka_year_proverka)
+            #         print('мы не обнаружили не го не месяц,поэтому создаём папку месяц и туда копируем файл')
+            #         os.makedirs(papka_year_mounth_proverka) #создаём папку по го и месяц если такой нет
+            #         shutil.copy(self.put_k_file, papka_year_mounth_proverka) #копируем файл в новую директорию
 
-            # if os.path.exists('сюда путь ')
-            # print(os.path.exists(papka_year_proverka)) #мы счиали дату каждого файла
-            # print(self.folder+'\\'+file)
-            # shutil.copy(put_k_file, new_papka) #копирует файл в раннее созданую папку
-            #тут сейчас думаю как запускать код от имени администратора?
+
+
 
     def remainder(self):# остаток файлов ( 120 из 453) и принтовать как файл обработается
         pass
+
+    def data_create_def(self):
+        from PIL import Image
+        from PIL.ExifTags import TAGS
+        imagename = self.put_k_file
+        image = Image.open(imagename)
+        exifData = image.getexif()
+        for tag_id in exifData:
+            tag = TAGS.get(tag_id,tag_id)
+            data = exifData.get(tag_id)
+            if isinstance(data, bytes):
+                data = data.decode()
+            vremen_dict ={}
+            vremen_dict[tag] = data
+            if 'DateTime' in vremen_dict: #потому что первый словарь не содержит дататиме и ломается прога
+                data_sozdaniya = vremen_dict['DateTime']
+                self.data_sozdaniya_year = data_sozdaniya[:4]
+                self.data_sozdaniya_mounth = data_sozdaniya[5:7]
+                print(self.data_sozdaniya_year)
+                print(self.data_sozdaniya_mounth)
+
 
     def __str__(self):
         return f'{self.papka_open}'
