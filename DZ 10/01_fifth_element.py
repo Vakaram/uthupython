f12c3b47ecc8beeca3ee93d7af5fc110e361b056
Vakaram <Vakaramm@gmail.com>
2022-07-18 17:45:08 +0500

    решил 2 задачки , а на 3 задачу 10 урока посмотрел только код и прочитал его 3 раза всё понятно. хороший код.

--- DZ 10/01_fifth_element.py
+++ DZ 10/01_fifth_element.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
-
-BRUCE_WILLIS = 42
-
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+#
+# BRUCE_WILLIS = 42
+#
+# input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+# leeloo = int(input_data[4])
+# result = BRUCE_WILLIS * leeloo
+# print(f"- Leeloo Dallas! Multi-pass № {result}!")
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
@@ -15,6 +15,32 @@ print(f"- Leeloo Dallas! Multi-pass № {result}!")
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
 
+# нужно указывать конкретную ошибку
+try:
+    BRUCE_WILLIS = 42
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS / leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError:
+    print(f'Не удалось выполнить преобразование наверное вы ввели буквы')
+except ZeroDivisionError:
+    print('Вероятно вы ввели 0 , на ноль делить нельзя')
+except NameError:
+    print('Нет такой переменной, кто писал этот код?!')
+except IndexError:
+    print('Вероятно вы не ввели достаточное количество символов введите не менее 5')
 
 
+# BRUCE_WILLIS = 42
+# input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+# leeloo = int(input_data[4])
+# result = BRUCE_WILLIS / leeloo
+# print(f"- Leeloo Dallas! Multi-pass № {result}!")
 
+# try:
+#     i = 0
+#     print(10 / i)
+#     print('сделано')
+# except ZeroDivisionError:  # указываем имя класса
+#     print('нельзя делить на ноль!')
--- DZ 10/02_groundhog_day.py
+++ DZ 10/02_groundhog_day.py
@@ -16,9 +16,29 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
-
+from random import randint
 ENLIGHTENMENT_CARMA_LEVEL = 777
-
-# TODO здесь ваш код
-
-# https://goo.gl/JnsDqu
+def one_day(carma_now=0):
+    ENLIGHTENMENT_CARMA_LEVEL = 777
+    all_day = 0
+    list_random = ['IamGodError','DrunkError','CarCrashError','GluttonyError','DepressionError','SuicideError',]
+    while carma_now <= ENLIGHTENMENT_CARMA_LEVEL:
+        add_karm = randint(1, 7)
+        carma_now += add_karm
+        print(f'Карма прибавилась на {add_karm} и стала {carma_now}')
+        all_day += 1
+        dice = randint(1,13)
+        if dice == 1:#или любое число чтобы была вероятность 1 к 13
+            try:
+                now_error = list_random[randint(0,5)] #выбираем рандомно наше исключение если выпало число 1
+                raise Exception(now_error)
+            except Exception as exc:
+                peremennaya_error = f'{exc.args}\n'
+                print(f'Наше исключение {peremennaya_error}')
+                file_log = open('logirovanie.txt','a',encoding= 'utf-8')
+                file_log.write(peremennaya_error)
+    else:
+        print(f'Мы выбрались из дня сурка кармы стало {carma_now}')
+        print(f'Дней затрачено {all_day}')
+day_surka = one_day()
+print(day_surka)
